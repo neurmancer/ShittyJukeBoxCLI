4d92a15c44da8d7d6ac195ab2d858b073200e515
neurmancer <m.arda.safak06@gmail.com>
2026-07-31 19:06:02 +0300

    I need to start commiting lyrics lol

--- ShittyJukeBox.c
+++ ShittyJukeBox.c
@@ -1,17 +1,50 @@
 //Fresh start time...
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <termios.h>
+
+//I am fucking paralyzed can't type anything...why?
+
+
+//She Loves Purple, So Do I.
+
+#define SECOND 1000000
+
+//ANSI DEFINES 
+
+#define FIX_FONT "\e[0m"
+#define WIPE_TERMINAL "\033[H\033[J"
+#define YET_ANOTHER_WIPE_TERMINAL "\033[3J"
+#define WIPE_TOP "\033[2J"
+#define GO_HOME "\033[H"
+#define VANISH_CURSOR "\033[?25l"
+#define BRING_BACK_THE_CURSOR_FROM_THE_DEAD "\033[?25h"
 
 
 
-
+void typewriter(char *lyrics);
 
 
 int main(void)
 {
+    srand(time(NULL));
+    setvbuf(stdout, NULL, _IONBF, 0);
 
-    //This is how it all started
-
-
+    
 
     return(0);
+}
+
+
+
+void typewriter( char *lyrics)
+{
+    char *iter = lyrics;
+    while (*iter != EOF) {
+        printf("%c",*iter);
+        iter++;
+    }
 }
--- src/songdata.h
+++ src/songdata.h
@@ -2,11 +2,15 @@
 #define SONGDATA_H
 
 typedef struct {
-    const char *title;
+    const char *title;  //8 on x86_64
     const char *lyrics;
     const char *url;
-    double duration;
+
+    float duration; //4*4 = 16 + 24 = 48 I guess optimzed the padding or shit dunno
+    float lyricsStartAt; 
+    float lyricsEndAt;  
     int writerType;
+
 } songMetaData;
 
 
@@ -38,14 +42,14 @@ songMetaData whiteGirlPopSongs[] = {0};
 /* ==================== DISPATCH TABLE ==================== */
 
 songMetaData *genreSongArrays[] = {
-    NULL,
+    (songMetaData *)0,  //Yeah I ain't typing NULL or nullptr lols
     myTherapySession,
     adhdSongs,
     nwobhmSongs,
     whiteGirlPopSongs
 };
 
-/* ==================== SPECIAL ONES (Yup me...no I didn't meanit...FUCK)  ==================== */
+/* ==================== SPECIAL ONES (Yup me...no I didn't mean it...FUCK)  ==================== */
 const char *rickroll = "";
 char *rickrollAudio = "https://drive.google.com/uc?export=open&id=10aiePDkX2t2XruCaFROIum9nGkQGUzth";
 char *roast = "https://drive.google.com/uc?export=open&id=1LXCZzIJ_LR9Thw8-M2oVnKfYCHUn5obY";
